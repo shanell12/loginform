10f73fc9d9cbee7588b22984d92675abf154499a
shanell <108282874+shanell12@users.noreply.github.com>
2022-11-09 05:37:53 -0800

    gjgf
    gjfjdjhf

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="style.css">
+  <title>Login Page in HTML with CSS Code Example</title>
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+
+
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous"><link rel="stylesheet" href="./style.css">
+
+</head>
+<body>
+<!-- partial:index.partial.html -->
+<div class="box-form">
+	<div class="left">
+		<div class="overlay">
+	<div class="wena">
+        <img src="image.png">
+</div>
+		</div>
+	</div>
+	
+	
+		<div class="right"><h5>چونەژوورەوە</h5>
+		<div class="rightt">
+		
+		
+		<div class="inputs">
+			<input type="text" placeholder="ناوی تەواو">
+			<br>
+			<input type="password" placeholder="وشەی نهێنی">
+		</div>
+			
+	
+	
+			<p>وشەی نهێنی لەبیرکردوە؟</p>
+		
+			
+	
+			<button type="submit">چونەژوورەوە</button>
+			
+	</div>
+	</div>
+</div>
+<!-- partial -->
+  
+</body>
+</html>
